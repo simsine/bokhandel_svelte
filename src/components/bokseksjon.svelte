c93ea487ef456eb4aad35615fc51bb3627767b7b
Simen <zimen2004@gmail.com>
2022-09-15 15:21:29 +0200

    small updates

--- src/components/bokseksjon.svelte
+++ src/components/bokseksjon.svelte
@@ -1,81 +1,91 @@
 <script>
-  export let bokliste;
+	export let bokliste;
 </script>
 
 <!-- BODY -->
-<h1 class="section-heading">{bokliste.seksjonHeading} - {bokliste.bøker.length} utvalgte titler</h1>
-<section>
-  {#each bokliste.bøker as bok}
-  <article>
-    <!--Forsidebilde-->
-    <a href="./boksider/"><img class="bok-forsidebilde" src="{bok.forsideBilde}" alt=""></a>
-    <!--Tittel-->
-    <h3 class="bok-tittel">{bok.tittel}</h3>
-    <!--Forfatter-->
-    <a href="http://" target="_blank">
-      <p class="bok-forfatter">{bok.forfatter}</p>
-    </a>
-    <!--Beskrivelse-->
-    <p class="bok-beskrivelse">{bok.beskrivelse}</p>
-  </article>
-  {/each}
+<h2 class="section-heading">{bokliste.seksjonHeading} - {bokliste.bøker.length} utvalgte titler</h2>
+<section id="{bokliste.seksjonHeading}">
+	{#each bokliste.bøker as bok}
+	<article>
+		<!--Forsidebilde-->
+		<a href="."><img class="bok-forsidebilde" src="{bok.forsideBilde}" alt=""></a>
+		<!--Tittel-->
+		<h3 class="bok-tittel">{bok.tittel}</h3>
+		<!--Forfatter-->
+		<a href="http://" target="_blank">
+			<p class="bok-forfatter">{bok.forfatter}</p>
+		</a>
+		<!--Beskrivelse-->
+		<p class="bok-beskrivelse">{bok.beskrivelse}</p>
+	</article>
+	{/each}
 </section>
 <hr>
 <!-- END OF BODY -->
 <style>
-  /* Section */
-  .section-heading {
-    margin-top: 10px;
-    margin-bottom: 10px
-  }
-  section {
-    margin-top: 15px;
-    margin-bottom: 15px;
-    display: grid;
-    grid-template-columns: repeat(4, 20%);
-    justify-items: center;
-    justify-content: space-around;
-    gap: 1rem;
-    row-gap: 1rem;
-  }
-  /* Media query */
-  @media screen and (max-width: 480px) {
-    section {
-      grid-template-columns: 80%;
-      row-gap: 0.5rem;
-    }
-  }
+	/* Section */
+	.section-heading {
+		margin-top: 10px;
+		margin-bottom: 10px
+	}
+	section {
+		margin-top: 15px;
+		margin-bottom: 15px;
+		display: grid;
+		grid-template-columns: repeat(4, 20%);
+		justify-items: center;
+		justify-content: space-around;
+		gap: 1rem;
+		row-gap: 1rem;
+	}
 
-  /* Cards */
-  section article {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    padding: 15px;
-    text-align: left;
+	/* Cards */
+	section article {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		padding: 15px;
+		text-align: left;
 
-    background-color: rgb(241,245,243);
-    border-radius: 0.5rem;
-  }
-  .bok-tittel, .bok-forfatter, .bok-beskrivelse{
-    margin-top: 0.75rem;
-    margin-bottom: 0rem;
-  }
-  .bok-beskrivelse {
-    font-size: small;
-    text-align: start;
-  }
-  .bok-beskrivelse::before{
-    content: '«';
-  }
-  .bok-beskrivelse::after{
-    content: '»';
-  }
-  .bok-forsidebilde {
-    justify-self: center;
-    width: 100%;
+		background-color: rgb(241,245,243);
+		border-radius: 0.5rem;
+	}
+	.bok-tittel, .bok-forfatter, .bok-beskrivelse{
+		margin-top: 0.75rem;
+		margin-bottom: 0rem;
+	}
+	.bok-beskrivelse {
+		font-size: small;
+		text-align: start;
+	}
+	.bok-beskrivelse::before{
+		content: '«';
+	}
+	.bok-beskrivelse::after{
+		content: '»';
+	}
+	.bok-forsidebilde {
+		justify-self: center;
+		width: 100%;
 
-    border-radius: 0.5rem;
-    box-shadow: 3px 5px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
-  }
+		border-radius: 0.5rem;
+		box-shadow: 3px 5px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
+	}
+
+	/* Media queries */
+	@media screen and (min-width: 550px) and (max-width: 900px) {
+		section {
+			grid-template-columns: repeat(3, 30%);
+			row-gap: 0.5rem;
+		}
+	}
+	@media screen and (max-width: 550px) {
+		section {
+			grid-template-columns: 85%;
+			row-gap: 1rem;
+		}
+		section article {
+			padding: 30px;
+		}
+	}
 </style>
--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,15 +1,41 @@
+<script>
+    // THIS SHIT DONT WORK LMAO
+
+    // let hideNavbar = false
+    // function toggleNavbar() {
+    //     let navElem = document.querySelector("nav")
+    //     if (hideNavbar == true) {
+    //         navElem.style.display = "none"
+    //         hideNavbar = false
+    //     }
+    //     else if (hideNavbar == false){
+    //         navElem.style.display = "flex"
+    //         hideNavbar = true
+    //     }
+    // }
+</script>
+
 <div class="logo-header">
-    <img src="../src/media/logo.gif" alt="Mas que papel logo" style="width: 25vw; display: inline-block;">
+    <img src="../src/media/logo.gif" alt="Mas que papel logo" style="width: 20vw; display: inline-block;">
     <h1 style="display:inline-block;">Bokhandel</h1>
+
+    <!-- <div style="display: flex; justify-content:flex-end; width:100%; margin-right:15px;">
+        <button on:click="{toggleNavbar()}">
+            <svg viewBox="0 0 75 40" width="40" height="40">
+                <rect width="75" height="10"></rect>
+                <rect y="20" width="75" height="10"></rect>
+                <rect y="40" width="75" height="10"></rect>
+            </svg>
+        </button>
+    </div> -->
 </div>
+
 <nav>
-    <a href="#anbefalt">Anbefalte bøker</a>
-    <a href="#japansk">Japanske bøker</a>
+    <a href="#Anbefalt!">Anbefalte bøker</a>
+    <a href="#Japansk">Japanske bøker</a>
     <a href="#andeby">Andeby</a>
     <a href="#eupåen">Europeisk på engelsk</a>
 </nav>
-
-<svelte:body />
 
 <style>
     .logo-header{
@@ -17,10 +43,14 @@
         align-items: center;
         margin: 5px;
     }
+    .logo-header:last-child{
+        justify-content: flex-end;
+    }
 
     nav{
         display: flex;
         flex-wrap: nowrap;
+        flex-direction: row;
         justify-content: space-around;
 
         box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
@@ -35,7 +65,6 @@
         transition-duration: 2s;
     }
     nav a:not(:last-child){
-
         border-right: 1px solid rgb(0 0 0 / 16%);
     }
     nav a:hover{
@@ -43,4 +72,17 @@
         flex:2;
     }
 
+    /* Media queries */
+    @media screen and (min-width: 550px) and (max-width: 900px) {
+		nav {
+            font-size: 75%;
+		}
+	}
+	@media screen and (max-width: 550px) {
+		nav {
+            flex-direction: column;
+			grid-template-columns: 85%;
+			row-gap: 1rem;
+		}
+	}
 </style>
