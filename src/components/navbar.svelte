ebd7d6aa279797ff18c10dcb01c4d0f04703db75
Simen <zimen2004@gmail.com>
2022-09-26 19:55:18 +0200

    added navbar toggle

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,18 +1,10 @@
 <script>
-    // THIS SHIT DONT WORK LMAO
 
-    // let hideNavbar = false
-    // function toggleNavbar() {
-    //     let navElem = document.querySelector("nav")
-    //     if (hideNavbar == true) {
-    //         navElem.style.display = "none"
-    //         hideNavbar = false
-    //     }
-    //     else if (hideNavbar == false){
-    //         navElem.style.display = "flex"
-    //         hideNavbar = true
-    //     }
-    // }
+    let navOpen = false
+
+    function toggleNavbar(){
+        navOpen = !navOpen;
+    }
 
     //window.TV2.toggleUserMenu=function(){e.style.display=e.style.display?null:"none",c.querySelector(".navbar-auth__button").setAttribute("aria-expanded",!e.style.display)}
 
@@ -22,18 +14,18 @@
     <img src="./media/logo.gif" alt="Mas que papel logo" style="width: 20vw; display: inline-block;">
     <h1 style="display:inline-block;">Bokhandel</h1>
 
-    <!-- <div style="display: flex; justify-content:flex-end; width:100%; margin-right:15px;">
-        <button on:click="{toggleNavbar()}">
+    <div style="display: flex; justify-content:flex-end; width:100%; margin-right:15px;">
+        <button on:click="{toggleNavbar}">
             <svg viewBox="0 0 75 40" width="40" height="40">
                 <rect width="75" height="10"></rect>
                 <rect y="20" width="75" height="10"></rect>
                 <rect y="40" width="75" height="10"></rect>
             </svg>
         </button>
-    </div> -->
+    </div>
 </div>
 
-<nav>
+<nav class:open={navOpen}>
     <a href="#Anbefalt!">Anbefalte bøker</a>
     <a href="#Japansk">Japanske bøker</a>
     <a href="#andeby">Andeby</a>
@@ -50,8 +42,18 @@
         justify-content: flex-end;
     }
 
+    button{
+        background: none;
+        color: inherit;
+        border: none;
+        padding: 0;
+        font: inherit;
+        cursor: pointer;
+        outline: inherit;
+    }
+
     nav{
-        display: flex;
+        display: none;
         flex-wrap: nowrap;
         flex-direction: row;
         justify-content: space-around;
@@ -73,6 +75,10 @@
     nav a:hover{
         background-color: rgba(128, 190, 210, 0.503);
         flex:2;
+    }
+
+    .open {
+        display: flex;
     }
 
     /* Media queries */
